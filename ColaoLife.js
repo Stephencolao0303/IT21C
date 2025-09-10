ceafdbaddaecc5f781f87c53089b65391ae3629a
Stephen <stephenbanaag0303@gmail.com>
2025-09-10 14:36:14 +0800

    Added student variables

--- ColaoLife.js
+++ ColaoLife.js
@@ -5,28 +5,3 @@ let year = "2nd Year";
 console.log("Name: "+ name);
 console.log("Course: "+ course);
 console.log("Year Level: "+ year);
-
-let sleep = 8
-let study = 2
-
-console.log("Total hours of sleep: "+ sleep);
-console.log("Total hours of study: "+ study);
-
-if (study>5) {
-    console.log("Great! You studied enough today.");
-} else {
-    console.log("You need to study more.");
-}
-
-let subjects = ["Web System", "Event-Driven","OOP"];
-console.log("List of my subjects this sem:");
-for (let i = 0; i < subjects.length; i++) {
-console.log(subjects [i]);
-}
-
-function studentSummary() {
-    console.log("My name is "+name+", a "+year+" from "+course);
-    return studentSummary;
-
-}
-console.log(studentSummary());
