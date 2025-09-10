84e46ec829d2bb00eeed3c90e0d643048b0b3ad3
Stephen <stephenbanaag0303@gmail.com>
2025-09-10 14:25:38 +0800

    Added Student variable

--- ColaoLife.js
+++ ColaoLife.js
@@ -1,20 +1,7 @@
-let name = "Stephen Colao "
+let name = "Stephen B. Colao"
 let course = "Bachelor of Science in Information Technology"
 let year = "2nd Year";
 
 console.log("Name: "+ name);
 console.log("Course: "+ course);
 console.log("Year Level: "+ year);
-
-
-let sleep = 8
-let study = 2
-
-console.log("Total hours of sleep: "+ sleep);
-console.log("Total hours of study: "+ study);
-
-if (study>5) {
-    console.log("Great! You studied enough today.");
-} else {
-    console.log("You need to study more.");
-}
