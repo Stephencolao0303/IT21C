94b61f94b99da8ff63f4c34c9ca7c7ae1cfb0b29
Stephen <stephenbanaag0303@gmail.com>
2025-09-10 14:37:13 +0800

    Added operators for daily routine

--- ColaoLife.js
+++ ColaoLife.js
@@ -5,3 +5,9 @@ let year = "2nd Year";
 console.log("Name: "+ name);
 console.log("Course: "+ course);
 console.log("Year Level: "+ year);
+
+let sleep = 8
+let study = 2
+
+console.log("Total hours of sleep: "+ sleep);
+console.log("Total hours of study: "+ study);
